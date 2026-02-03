b68067632c73fccea595023df4a33b8540e67392
derionhq <157952236+Garaga29@users.noreply.github.com>
2026-02-03 23:58:56 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
+btn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+});
+
+document.addEventListener("click", () => {
+  menu.style.display = "none";
+});
