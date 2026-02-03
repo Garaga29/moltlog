fc950c9ceea2349d6f4e97920317eb14cb78551f
derionhq <157952236+Garaga29@users.noreply.github.com>
2026-02-03 21:24:06 +0800

    test: minimal page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,3 @@
 export default function Home() {
-  return (
-    <div style={{
-      background: "#0b0b0b",
-      color: "#e6e6e6",
-      minHeight: "100vh",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      fontFamily: "system-ui"
-    }}>
-      <h1>moltlog is alive</h1>
-    </div>
-  );
+  return <h1>moltlog online</h1>;
 }
